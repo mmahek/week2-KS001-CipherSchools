5bd8b51c59bb2596e3492b8713889cbcb4d0183e
Mahek <114944166+mmahek@users.noreply.github.com>
2023-10-17 18:19:03 +0530

    Lecture 8

---
+++ Lecture8.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+/*
+    Importance of Functions:
+    1. Divide and conquer: Construct a program from smaller pieces or components 
+
+    2. Reuse: write once and use that function again and again in the program
+
+    3. Abstraction: Hide internal details e.g. library function
+
+    4. To improve readability of code
+
+    5. Reduces the size of the code, duplicate set of statement are replaced by function calls. 
+
+*/
+int main(){ //main function
+    return 0;
+}
