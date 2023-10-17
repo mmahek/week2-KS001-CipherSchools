91ef4861e9bee6d966ab9c0f88ca74b580f8d178
Mahek <114944166+mmahek@users.noreply.github.com>
2023-10-17 18:24:28 +0530

    Lecture 8 part 2

---
+++ Lecture8_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b){
+    int c;
+    c = a + b;
+    return c;
+}
+int main(){
+    int a, b;
+    a = 3;
+    b = 5;
+    int s = sum(a,b);
+    cout << s;
+    return 0;
+}
